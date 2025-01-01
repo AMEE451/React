b490e1db4745064f39a069eabec171297ad60e3f
AMEE451 <desaiamee699@gmail.com>
2025-01-01 16:23:19 +0530

    Counter-State

--- counter/src/counter.jsx
+++ counter/src/counter.jsx
@@ -1,29 +1,24 @@
+
 import React, { useState } from "react";
 
-const Counter=({initial})=>{
-    let [count,setCount]=useState(initial)
+const Counter = ({ initial }) => {
+    let [count, setCount] = useState(initial)
 
-    // const remove=()=>{
-    //     setCount(count-1)
-    // }
-    // const add=()=>{    
-    //     setCount(count+1)
-    // }
-
-    const handlecounter=(opr,value)=>{
-        if(opr=="+"){
-            setCount(count+value)
+    const handlecounter = (opr, value) => {
+        if (opr === "+") {
+            setCount(count + value)
         }
-        else{
-            setCount(count-value)
+        else {
+            setCount(count - value)
         }
     }
 
     return (
         <div>
-            <button onClick={()=>handlecounter("+",5)}>-</button>
+            <h1>Counter</h1>
+            <button onClick={() => handlecounter("-", 5)}>-</button>
             <button>{count}</button>
-            <button onClick={()=>handlecounter("-",5)}>+</button>
+            <button onClick={() => handlecounter("+", 5)}>+</button>
         </div>
     )
 }
